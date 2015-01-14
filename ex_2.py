611ae9d48adfd05fb08a1bd2ece5bf57658e211b
Hackbright Student <info@hackbrightacademy.com>
2015-01-14 15:27:05 -0800

    Initial commit

---
+++ ex_2.py
@@ -0,0 +1,32 @@
+print "I will now count my chickens:"
+
+# adds <- divides
+print "Hens", float(25 + 30 / 6)
+# multiply, mod, subtract
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+
+# mod, divide, subt/add
+print float(3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4.0 + 6)
+
+# 3 + 2 + 1 - 5 + 0 - 0.75 + 6
+#7
+
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# add, sub, checks boolean
+# 5 < -2
+print 3 + 2 < 5 - 7
+
+# prints value of result b/c in print line
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
